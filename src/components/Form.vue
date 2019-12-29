36d95a186249f486574bdc2c7faddb6f125fe57e
kevhunte <kevinmhunte@gmail.com>
2019-12-29 12:24:50 -0500

    Accordian for algorithm options

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -31,14 +31,26 @@ a {
 
   <div class="Enc-Engines">
     <strong>Select a Cipher to Get Started:</strong>
-
+    <br>
     <b-dropdown v-b-popover.hover.top="'Symmetric Algorithms'" id="dropdown-1" text="Cipher Engines" class="m-md-2">
-      <b-dropdown-item @click="setEngine('AES')">AES</b-dropdown-item>
-      <b-dropdown-item @click="setEngine('Rij')">Rijndael</b-dropdown-item>
-      <b-dropdown-item @click="setEngine('Blow')">Blowfish</b-dropdown-item>
-      <b-dropdown-item @click="setEngine('RC4')">RC4</b-dropdown-item>
+      <b-dropdown-item v-b-toggle.AES-1 @click="setEngine('AES')">AES</b-dropdown-item>
+      <b-dropdown-item v-b-toggle.Rij-1 @click="setEngine('Rij')">Rijndael</b-dropdown-item>
+      <b-dropdown-item v-b-toggle.Blow-1 @click="setEngine('Blow')">Blowfish</b-dropdown-item>
+      <b-dropdown-item v-b-toggle.RC4-1 @click="setEngine('RC4')">RC4</b-dropdown-item>
     </b-dropdown>
-    {{engine}}
+    <!--Link multiple Id's on v-b-toggle for sequential collapses-->
+    <b-collapse id="AES-1" accordion="alg-accordion" class="mt-2">
+      <p class="card-text">AES contents Here: IV, Salt Size, Padding, Key Size</p>
+    </b-collapse>
+    <b-collapse id="Rij-1" accordion="alg-accordion" class="mt-2">
+      <p class="card-text">Rij contents Here: Block, IV, Salt Size, Padding, Key Size</p>
+    </b-collapse>
+    <b-collapse id="Blow-1" accordion="alg-accordion" class="mt-2">
+      <p class="card-text">Blow contents Here: IV, Salt Size, Padding, Key Size</p>
+    </b-collapse>
+    <b-collapse id="RC4-1" accordion="alg-accordion" class="mt-2">
+      <p class="card-text">RC4 contents Here: IV, Salt Size, Padding, Key Size</p>
+    </b-collapse>
   </div>
 </div>
 </template>
